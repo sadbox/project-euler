f1202521113615ada220a700a3ecb16e141a8f6f
James McGuire <james@sadbox.org>
2013-07-05 15:16:18 -0500

    Finished problem 3

--- src/euler/core.clj
+++ src/euler/core.clj
@@ -1,6 +1,7 @@
 (ns euler.core
   (:require [euler.euler-1 :as euler1]
-            [euler.euler-2 :as euler2])
+            [euler.euler-2 :as euler2]
+            [euler.euler-3 :as euler3])
   (:gen-class))
 
 (defn -main
@@ -10,4 +11,5 @@
   (println (euler1/euler-bo) (euler1/euler-aaron))
   (println "Problem 2:")
   (println (euler2/euler 4000000))
-  (println "Problem 3:"))
+  (println "Problem 3:")
+  (println (euler3/euler 600851475143)))
--- src/euler/euler_3.clj
+++ src/euler/euler_3.clj
@@ -1,1 +1,10 @@
-(ns euler.euler-3)
+(ns euler.euler-3
+  (:require [clojure.contrib.lazy-seqs :as lazyseqs]))
+
+(defn primes-reversed
+  [number]
+  (reverse (take-while #(< % number) lazyseqs/primes)))
+
+(defn euler
+  [number]
+  (first (filter #(zero? (mod number %)) (primes-reversed (int (Math/sqrt number))))))
