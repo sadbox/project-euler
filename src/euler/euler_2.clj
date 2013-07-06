943c1dad790ad98c67cadc953c3b19345ef2b1f8
James McGuire <james@sadbox.org>
2013-07-06 08:09:12 -0500

    euler_2 is not using the common fib-seq generator

--- src/euler/euler_2.clj
+++ src/euler/euler_2.clj
@@ -1,11 +1,7 @@
-(ns euler.euler-2)
-
-(defn fib-seq []
-  (( fn rfib [a b]
-    (lazy-seq (cons a (rfib b (+ a b)))))
-  0 1))
+(ns euler.euler-2
+  (:require [euler.util :as util]))
 
 (defn euler [maxnum]
   (apply +
     (take-while #(< % maxnum)
-      (filter #(zero? (mod % 2)) (fib-seq)))))
+      (filter #(zero? (mod % 2)) util/fib-seq))))
